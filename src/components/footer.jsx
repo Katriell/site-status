3339fa06beb0efcc326ceb3a4bb046b98da2f927
Lisita <60808770+Katriell@users.noreply.github.com>
2024-09-27 19:23:29 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <p>
           <CustomLink
             text={Package.alia}
-            to="https://github.com/imsyy/site-status"
+            to="https://github.com/Katriell/site-status"
           />
           &nbsp;Version&nbsp;{Package.version}
         </p>
@@ -36,11 +36,11 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://blog.satania.work" text="秦心心" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
-              <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
+              <CustomLink to="https://icp.gov.moe/" text={siteIcp} />
             </React.Fragment>
           ) : null}
         </p>
